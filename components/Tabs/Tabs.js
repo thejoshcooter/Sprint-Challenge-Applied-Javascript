8c1ef96958ffb6183eda7bc38b5333f1d7efb323
josh cooter <thejoshcooter@gmail.com>
2019-04-12 10:51:33 -0500

    fix error on display flex

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -50,7 +50,7 @@ class TabCard {
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-     this.cardElement.style.display = flex;
+     this.cardElement.style.display = 'flex';
   }
 
 }
